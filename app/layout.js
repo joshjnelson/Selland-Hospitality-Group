70ba03bca8f134315d0e1b1f4c6f19c8efa3e59a
ntrpx <m@ntrpx.com>
2026-04-29 12:26:08 -0700

    Add Google Analytics (GA4: G-NXCGS4L248)

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
+import Script from "next/script";
 import "./globals.css";
+
+const GA_ID = "G-NXCGS4L248";
 
 export const metadata = {
   title: "Selland Hospitality Group",
@@ -9,6 +12,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
     </html>
   );
 }
